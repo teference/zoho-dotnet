ed3a8b8912d3299b96579ff6a8051eaef8f99d8d
Jaspalsinh Chauhan <jachauhan@gmail.com>
2016-07-06 01:16:09 +0530

    Publish nuget package and nuspec related changes

--- source/Zoho.Api/Properties/AssemblyInfo.cs
+++ source/Zoho.Api/Properties/AssemblyInfo.cs
@@ -1,10 +1,27 @@
-﻿using System.Resources;
+﻿#region Copyright Teference
+// ************************************************************************************
+// <copyright file="AssemblyInfo.cs" company="Teference">
+// Copyright © Teference 2016. All right reserved.
+// </copyright>
+// ************************************************************************************
+// <author>Jaspalsinh Chauhan</author>
+// <email>jachauhan@gmail.com</email>
+// <project>Teference - Zoho API C#.NET SDK</project>
+// ************************************************************************************
+#endregion
+
+#region Namespace
+
 using System.Reflection;
+using System.Resources;
+
+#endregion
 
 [assembly: AssemblyTitle("Zoho.Api")]
 [assembly: AssemblyDescription("Zoho Rest API - .NET SDK")]
-[assembly: AssemblyProduct("Zoho.Api")]
+[assembly: AssemblyCompany("Teference")]
+[assembly: AssemblyProduct("Zoho API C#.NET SDK")]
 [assembly: AssemblyCopyright("Copyright © Teference 2016")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.1")]
+[assembly: AssemblyFileVersion("0.6.1")]
